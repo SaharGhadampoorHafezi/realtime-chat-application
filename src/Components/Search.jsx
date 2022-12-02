4d082e7a514a71f835212b07a0b97eb144e6c1e9
SaharGhadampoorHafezi <saharghdampoor@gmail.com>
2022-12-02 19:15:37 +0330

    Search User

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,21 +1,57 @@
-import React from 'react'
-import '../style.scss'
-import profile from '../images/photo for profile.jpg'
+import React from "react";
+import "../style.scss";
+import profile from "../images/photo for profile.jpg";
+import { useState } from "react";
+import { db } from "../firebase";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 const Search = () => {
+  const [userName, setUserName] = useState("");
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "===", userName)
+    );
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
+  
+
   return (
     <div className="search">
-        <div className="searchForm">
-            <input type="text" placeholder='Find a user'/>
-        </div>
+      <div className="searchForm">
+        <input
+          type="text"
+          placeholder="Find a user"
+          onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKey}
+        />
+      </div>
+      {error && <p>User not found</p>}
+      {user && (
         <div className="userChat">
-            <img src={profile} alt="" />
-            <div className="userChatInfo">
-                <span>Jane</span>
-            </div>
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
         </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,18 +21,6 @@ const Login = () => {
     }
   };
   return (
-    // <div className="formContainer">
-    //     <div className="formWrapper">
-    //         <span className='logo'>Lama Chat</span>
-    //         <span className="title">Register</span>
-    //         <form action="">
-    //             <input type="email" placeholder='email'/>
-    //             <input type="password" placeholder='password'/>
-    //             <button>Sign ip</button>
-    //         </form>
-    //         <p>You do not have an account? Register</p>
-    //     </div>
-    // </div>
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Lama Chat</span>
